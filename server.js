f426f3a5482bf7c737a91c1ece44071d75cd9431
Rich Capuy <rccapuy@gmail.com>
2020-07-01 23:19:39 -0400

    html-routes moved to routes folder

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,18 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 // routes
 // require("./routes/api-routes.js")(app);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
